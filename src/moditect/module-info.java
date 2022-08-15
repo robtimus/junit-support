f4094af57456f07703a28634ead957c8d84ded7a
Rob Spoor <rob@icemanx.nl>
2022-08-15 17:28:53 +0200

    Removed TempDirUtils, as it can be easily replaced with calls to methods from Files

--- src/moditect/module-info.java
+++ src/moditect/module-info.java
@@ -10,7 +10,6 @@ module com.github.robtimus.junit.support {
     exports com.github.robtimus.junit.support.collections;
     exports com.github.robtimus.junit.support.collections.annotation;
     exports com.github.robtimus.junit.support.extension;
-    exports com.github.robtimus.junit.support.extension.tempdir;
     exports com.github.robtimus.junit.support.extension.testresource;
     exports com.github.robtimus.junit.support.io;
     exports com.github.robtimus.junit.support.matchers.nio.file;
