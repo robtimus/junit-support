fc01aa3c1529e33e6cc251830b5886ddfcb0c744
Rob Spoor <rob@icemanx.nl>
2022-08-23 17:36:35 +0200

    Removed PathMatchers again

--- src/moditect/module-info.java
+++ src/moditect/module-info.java
@@ -9,7 +9,6 @@ module com.github.robtimus.junit.support {
     exports com.github.robtimus.junit.support;
     exports com.github.robtimus.junit.support.extension;
     exports com.github.robtimus.junit.support.extension.testresource;
-    exports com.github.robtimus.junit.support.matchers.nio.file;
     exports com.github.robtimus.junit.support.params;
     exports com.github.robtimus.junit.support.test;
     exports com.github.robtimus.junit.support.test.collections;
